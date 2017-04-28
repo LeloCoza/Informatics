f4a5916ec37e7f1667566db3e56e498845e31c73
Zombulelo <nombulelojk@outlook.com>
2017-04-28 16:04:28 +0200

    created methods to execute Insert/Update/Delete and Select statements using SQL command and Dataset.

--- DatabaseLayer/clsDataLayer.cs
+++ DatabaseLayer/clsDataLayer.cs
@@ -13,5 +13,24 @@ namespace DatabaseLayer
     public class clsDataLayer
     {
         private string conn = ConfigurationManager.ConnectionStrings["FishLandDBEntities"].ToString();
+
+        public void InsertUpdateDeleteSQLString(string sqlstring) {
+
+            SqlConnection objsqlconn = new SqlConnection(conn);
+            objsqlconn.Open();
+            SqlCommand objcmd = new SqlCommand(sqlstring, objsqlconn);
+            objcmd.ExecuteNonQuery();
+        }
+
+        public object ExecuteSqlString(string sqlstring) {
+
+            SqlConnection objsqlconn = new SqlConnection(conn);
+            objsqlconn.Open();
+            DataSet ds = new DataSet();
+            SqlCommand objcmd = new SqlCommand(sqlstring, objsqlconn);
+            SqlDataAdapter objAdp = new SqlDataAdapter(objcmd);
+            objAdp.Fill(ds);
+            return ds;
+        }
     }
 }
