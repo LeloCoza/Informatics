21216df42cbff19d5b5f25c46b96584aa2688027
Zombulelo <nombulelojk@outlook.com>
2017-04-28 15:44:56 +0200

    imported data references in database layer library and called the connection string "FishLandDBEntities"  in  database layer file.

--- DatabaseLayer/clsDataLayer.cs
+++ DatabaseLayer/clsDataLayer.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
 namespace DatabaseLayer
 {
     public class clsDataLayer
     {
+        private string conn = ConfigurationManager.ConnectionStrings["FishLandDBEntities"].ToString();
     }
 }
